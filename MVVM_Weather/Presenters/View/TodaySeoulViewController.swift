2d0d5eec81cfd34afb1224a10722a76b47edf2e3
juyeong ko <juko95@me.com>
2023-03-10 15:43:51 +0900

    feat: configure UI

---
+++ MVVM_Weather/Presenters/View/TodaySeoulViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherViewController.swift
+//  MVVM_Weather
+//
+//  Created by juyeong koh on 2023/03/10.
+//
+
+import UIKit
+
+
+class TodaySeoulViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var todayTemperatureLabel: UILabel!
+    @IBOutlet weak var todayStateLabel: UILabel!
+    
+    // MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ MVVM_Weather/Presenters/View/TomorrowLondonViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TommorowLondonViewController.swift
+//  MVVM_Weather
+//
+//  Created by juyeong koh on 2023/03/10.
+//
+
+import UIKit
+
+
+class TomorrowLondonViewController: UIViewController {
+    
+    // MARK: - Properties
+
+    @IBOutlet weak var tomorrowTempLabel: UILabel!
+    @IBOutlet weak var tomorrowStateLabel: UILabel!
+    
+    // MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
